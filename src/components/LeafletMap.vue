cd0b3d4cb71c48b80f2e60b01ff1738460394356
prismzdrive <145449829+RadomirTikkerbar@users.noreply.github.com>
2025-06-08 20:12:32 +0300

    Update LeafletMap.vue

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,36 +1,31 @@
 <script setup>
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { onMounted, useId, watch } from 'vue';
+import { onMounted, useId } from 'vue';
 
-let { center, zoom } = defineProps(['center', 'zoom']);
+let { center } = defineProps(['center'])
 
 let id = 'map-' + useId();
-let map;
+
 onMounted(() => {
     console.log(document.getElementById(id));
 
-    map = L.map(id).setView(center, zoom);
+    var map = L.map(id).setView(center, 16);
+
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 });
 
-watch(() => center, (newCenter, oldCenter) => {
-    console.log(newCenter, oldCenter);
-    map.panTo(newCenter);
-});
-watch(() => zoom, newZoom => {
-    map.setZoom(newZoom);
-});
 </script>
 <template>
-     <div :id="id"></div>
+<div :id="id"></div>
 </template>
-
 <style scoped>
+    
 div { 
-    height: 40vh;
-}
+    height: 40vh; 
+    }
+
 </style>
